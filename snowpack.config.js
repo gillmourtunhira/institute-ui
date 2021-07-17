4e8394d9a3a0894d37d6b2c7bdc4f9c6ece6b359
gillmourtunhira <gillmourtunhira@gmail.com>
2021-07-18 00:52:41 +0200

    theme ui files structure initial

---
+++ snowpack.config.js
@@ -0,0 +1,25 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    /* ... */
+    // directory name: 'build directory'
+    public: '/',
+    src: '/dist',
+  },
+  plugins: [
+    /* ... */
+    '@snowpack/plugin-svelte',
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script></script>
+<style></style>
+<div class="App">
+    <div class="wrapper">
+        <h3>Snowpack + Svelte !</h3>
+    </div>
+</div>
